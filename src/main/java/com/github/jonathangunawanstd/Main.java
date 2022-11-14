fc531282d739c07ccad60e6283a2ac8c5e89b883
jonathan <jonathangunawanstd@gmail.com>
2022-11-14 14:46:47 +0800

    Change message on develop branch

--- src/main/java/com/github/jonathangunawanstd/Main.java
+++ src/main/java/com/github/jonathangunawanstd/Main.java
@@ -1,27 +1,5 @@
-package com.github.jonathangunawanstd;
-
-public class Main {
+package com.github.jonathangunawanstd;public class Main {
     public static void main(String[] args) {
-        plugins {
-    id 'java'
-}
-
-group 'com.github.jonathangunawanstd'
-version '1.0-SNAPSHOT'
-
-            compileJava.options.encoding = 'UTF-8’
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation 'org.junit.jupiter:junit-jupiter-api:5.8.1'
-    testRuntimeOnly 'org.junit.jupiter:junit-jupiter-engine:5.8.1'
-}
-
-test {
-    useJUnitPlatform()
-}
+        System.out.println("Hi world!");
     }
 }
