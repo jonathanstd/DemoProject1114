a6ad594f2534ce4909b6e7439067d5b84f148ca2
jonathan <jonathangunawanstd@gmail.com>
2022-11-14 15:25:58 +0800

    CustomCalculator

---
+++ src/main/java/com/github/jonathangunawanstd/CustomCalculator.java
@@ -0,0 +1,9 @@
+package com.github.jonathangunawanstd;
+
+public class CustomCalculator {
+    public int add(int a, int b){
+        return a + b;
+    }
+}
+
+
--- src/main/java/com/github/jonathangunawanstd/Main.java
+++ src/main/java/com/github/jonathangunawanstd/Main.java
@@ -2,6 +2,6 @@ package com.github.jonathangunawanstd;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hi Java");
     }
 }
