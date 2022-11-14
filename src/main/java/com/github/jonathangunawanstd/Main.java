1a8938b0b88459203717d16750b3d1745cabf598
jonathan <jonathangunawanstd@gmail.com>
2022-11-14 14:52:12 +0800

    Change message on master branch

--- src/main/java/com/github/jonathangunawanstd/Main.java
+++ src/main/java/com/github/jonathangunawanstd/Main.java
@@ -1,5 +1,7 @@
-package com.github.jonathangunawanstd;public class Main {
+package com.github.jonathangunawanstd;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Hello Java");
     }
 }
